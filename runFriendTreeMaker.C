6c826b7887ad243acc77e06c28046a87b9f37f48
Justin Hugon <jhugon@fnal.fake>
2016-10-27 15:05:42 -0500

    updates to work with new v1 samples

--- runFriendTreeMaker.C
+++ runFriendTreeMaker.C
@@ -3,9 +3,9 @@
 
   unsigned maxEvents = 100000000;
 
-  makeFriendTree("anaTree_pip_v11.root","friendTree_pip_v11.root",maxEvents);
-  makeFriendTree("anaTree_p_v10.root","friendTree_p_v10.root",maxEvents);
-  makeFriendTree("anaTree_mup_v10.root","friendTree_mup_v10.root",maxEvents);
-  makeFriendTree("anaTree_kp_v10.root","friendTree_kp_v10.root",maxEvents);
+  makeFriendTree("anaTree_pip_v1.root","friendTree_pip_v1.root",maxEvents);
+  makeFriendTree("anaTree_p_v1.root","friendTree_p_v1.root",maxEvents);
+  //makeFriendTree("anaTree_mup_v10.root","friendTree_mup_v10.root",maxEvents);
+  //makeFriendTree("anaTree_kp_v10.root","friendTree_kp_v10.root",maxEvents);
 
 }
