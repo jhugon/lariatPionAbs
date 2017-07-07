6c5a9c12a4846afeb597f95851d0eee821cb8dff
Justin Hugon <jhugon@fnal.fake>
2017-07-06 23:10:55 -0500

    tofRooFit.py working fit now

--- tofRooFit.py
+++ tofRooFit.py
@@ -4,24 +4,45 @@ from ROOT import gStyle as gStyle
 root.gROOT.SetBatch(True)
 from helpers import *
 
-def fitMass2(c,do_toy_data=False,plot_components=False):
+def fitMass2(c,do_toy_data=False,plot_components=True,binned=True):
 
     workspace = root.RooWorkspace("w")
     mass = root.RooRealVar("mass","Mass [MeV]",0,2000.)
-    mass2 = root.RooRealVar("mass2","Mass Squared [MeV^{2}]",-2e5,3e6)
-    true_p = root.RooRealVar("reco_momo","True Momentum [MeV]",500,0,2000.)
+    mass2 = root.RooRealVar("mass2","Mass Squared [MeV^{2}]",-2e5,2e6)
+    true_p = root.RooRealVar("reco_momo","True Momentum [MeV]",500,0,1500.)
     observables = root.RooArgSet(mass2)
+    mass.setBins(100)
+    mass2.setBins(200)
+    true_p.setBins(50) # speeds up data-avaraging projection
 
     data = None
+    Ndata = 1.
     if not do_toy_data:
         infile = root.TFile("momentumTest.root")
         intree = infile.Get("lowlevel/Mass Tree");
         data = root.RooDataSet("data","data",root.RooArgSet(mass2,mass,true_p),root.RooFit.Import(intree))
+        #data = data.reduce("reco_momo >600 && reco_momo < 605")
+        Ndata = data.sumEntries()
+        if binned:
+            data = data.binnedClone()
 
     d = root.RooRealVar("d","Distance",6.683)
-    true_p.setBins(50) # speeds up data-avaraging projection
     sigma_p = root.RooRealVar("sigma_p","",50.)
     sigma_dt = root.RooRealVar("sigma_dt","",0.5)
+    shift_dt2 = root.RooRealVar("shift_dt2","Shift of #Delta t Squared [ns^{2}]",74,-100,200)
+    coef1_p = root.RooRealVar("coef1_p","1st Polynomial Coefficient for Momentum",1.086,1.,1.50)
+
+    # Derived parameters
+    d2 = root.RooFormulaVar("d2","Distance^{2} [m^{2}]","pow(@0,2)",root.RooArgList(d))
+    true_p2 = root.RooFormulaVar("true_p2","True Momentum Squared [MeV^{2}]",
+                         "pow(@0,2)",
+                         root.RooArgList(true_p))
+    measured_p = root.RooFormulaVar("measured_p","Measured Momentum [MeV]",
+                         "@0*@1",
+                         root.RooArgList(true_p,coef1_p))
+    measured_p2 = root.RooFormulaVar("measured_p2","Measured Momentum Squared [MeV^{2}]",
+                         "pow(@0,2)",
+                         root.RooArgList(measured_p))
 
     # Make true momentum distribution
     true_p_norm1 = root.RooRealVar("true_p_norm1","",0.02)
@@ -39,12 +60,12 @@ def fitMass2(c,do_toy_data=False,plot_components=False):
     true_p_distribution = root.RooAddPdf("true_p_distribution","True Momentum Distribution",root.RooArgList(true_p_gaus1,true_p_gaus2,true_p_gaus3),root.RooArgList(true_p_norm1,true_p_norm2,true_p_norm3))
 
     particleConfigs = [
-      #("electron","Electron",0.511,0.1),
-      ("muon","Muon",105.658,0.1),
-      ("pion","Pion",139.57,0.1),
-      ("kaon","Kaon",493.677,0.005),
-      ("proton","Proton",938.27,0.3),
-      #("Deuteron","Deuteron",1875.6,0.002),
+      ("electron","Electron",0.511,0.01*Ndata),
+      ("muon","Muon",105.658,0.03*Ndata),
+      ("pion","Pion",139.57,0.03*Ndata),
+      ("kaon","Kaon",493.677,0.007*Ndata),
+      ("proton","Proton",938.27,0.8*Ndata),
+      ("Deuteron","Deuteron",1875.6,0.002*Ndata),
     ]
 
     gaussians = []
@@ -53,32 +74,27 @@ def fitMass2(c,do_toy_data=False,plot_components=False):
 
     allVars = []
     for particle_name, particle_title, particle_mass, particle_fraction in particleConfigs:
-        fraction = root.RooRealVar("fraction_"+particle_name,"Fraction of "+particle_title,particle_fraction,0.,1.)
-
+        fraction = root.RooRealVar("N_"+particle_name,"Number of of "+particle_title,particle_fraction,0.,Ndata)
         true_mass = root.RooRealVar("true_mass_"+particle_name,"True Mass of "+particle_title,particle_mass)
-
-        true_dt = root.RooFormulaVar("true_dt_"+particle_name,"True #Delta t [ns]",
-                             "sqrt(@2*@2/0.29979/0.29979*(1+@1*@1/@0/@0))",
-                             root.RooArgList(true_p,true_mass,d))
 
         true_mass2 = root.RooFormulaVar("true_mass2_"+particle_name,"True Mass Squared [MeV^{2}]",
                              "pow(@0,2)",
                              root.RooArgList(true_mass))
 
-        true_p2 = root.RooFormulaVar("true_p2_"+particle_name,"True Momentum Squared [MeV^{2}]",
-                             "pow(@0,2)",
-                             root.RooArgList(true_p))
+
         true_dt2 = root.RooFormulaVar("true_dt2_"+particle_name,"True #Delta t Squared [ns^{2}]",
-                             "pow(@0,2)",
-                             root.RooArgList(true_dt))
+                             "@2/0.29979/0.29979*(1+@1/@0)",
+                             root.RooArgList(true_p2,true_mass2,d2))
+        true_dt = root.RooFormulaVar("true_dt_"+particle_name,"True #Delta t [ns]",
+                             "sqrt(@0)",
+                             root.RooArgList(true_dt2))
+
         variance_p = root.RooFormulaVar("variance_p_"+particle_name,"Variance of Momentum [MeV^{2}]",
                              "pow(@0,2)",
                              root.RooArgList(sigma_p))
         variance_dt = root.RooFormulaVar("variance_dt_"+particle_name,"Variance of #Delta t [ns^{2}]",
                              "pow(@0,2)",
                              root.RooArgList(sigma_dt))
-
-        d2 = root.RooFormulaVar("d2_"+particle_name,"Distance^{2} [m^{2}]","pow(@0,2)",root.RooArgList(d))
 
         variance_mass = root.RooFormulaVar("variance_mass_"+particle_name,"Mass Variance [MeV^4]",
                              "@4/@2*pow((@0+@1)/@5,2) + @0/@1*@3",
@@ -96,6 +112,17 @@ def fitMass2(c,do_toy_data=False,plot_components=False):
                              "sqrt(@0)",
                              root.RooArgList(variance_mass2))
 
+        mean_dt2 = root.RooFormulaVar("mean_dt2_"+particle_name,"Mean of #Delta t Squared [ns^{2}]",
+                             "@0+@1",
+                             root.RooArgList(true_dt2,shift_dt2));
+        
+        mean_mass2 = root.RooFormulaVar("mean_mass2_"+particle_name,"Mean Mass Squared [MeV^2]",
+                             "@0*(@1/@2*0.29979*0.29979-1)",
+                             root.RooArgList(measured_p2,mean_dt2,d2))
+        mean_mass = root.RooFormulaVar("mean_mass_"+particle_name,"Mean Mass [MeV]",
+                             "sqrt(@0)",
+                             root.RooArgList(mean_mass2))
+
         true_mass2.Print()
         true_p2.Print()
         true_dt2.Print()
@@ -106,14 +133,18 @@ def fitMass2(c,do_toy_data=False,plot_components=False):
         sigma_mass.Print()
         variance_mass2.Print()
         sigma_mass2.Print()
+        mean_dt2.Print()
+        mean_mass2.Print()
+        mean_mass.Print()
 
-        gauss = root.RooGaussian("gauss_"+particle_name,particle_name,mass,true_mass,sigma_mass);
-        gauss2 = root.RooGaussian("gauss2_"+particle_name,particle_name,mass2,true_mass2,sigma_mass2);
+        gauss = root.RooGaussian("gauss_"+particle_name,particle_name,mass,mean_mass,sigma_mass);
+        gauss2 = root.RooGaussian("gauss2_"+particle_name,particle_name,mass2,mean_mass2,sigma_mass2);
         gaussians.append(gauss)
         gaussians2.append(gauss2)
         fractions.append(fraction)
-        wImport = getattr(workspace,"import")
-        wImport(gauss)
+        #wImport = getattr(workspace,"import")
+        #wImport(gauss)
+        #wImport(gauss2)
         l = locals()
         for k in l:
           allVars.append(l[k])
@@ -135,6 +166,10 @@ def fitMass2(c,do_toy_data=False,plot_components=False):
     if do_toy_data:
         toy_data = model_mass_momentum.generate(root.RooArgSet(mass,true_p),5000.)
         toy_data2 = model_mass2_momentum.generate(root.RooArgSet(mass2,true_p),5000.)
+    else:
+        model2.fitTo(data,
+                    root.RooFit.ConditionalObservables(root.RooArgSet(true_p)),
+                    root.RooFit.NumCPU(4))
 
     c.SetRightMargin(0.1)
 
@@ -187,42 +222,42 @@ def fitMass2(c,do_toy_data=False,plot_components=False):
         leg = drawNormalLegend(gaus_graphs,gaus_titles,option="l",position=(0.55,0.7,0.85,0.89))
     c.SaveAs("TOFFit2_zoom.png")
     c.SaveAs("TOFFit2_zoom.pdf")
-
-    gaus_graphs = []
-    gaus_titles = []
-    frame = mass.frame(root.RooFit.Title(""))
-    #frame.updateNormVars(root.RooArgSet(mass,true_p)) # makes RooFit contionalize on true_p
-    if do_toy_data:
-        toy_data.plotOn(frame)
-        model.plotOn(frame,root.RooFit.ProjWData(toy_data,True))
-    else:
-        data.plotOn(frame)
-        model.plotOn(frame,root.RooFit.ProjWData(data,True))
-    gaus_graphs.append(frame.getObject(int(frame.numItems())-1))
-    gaus_titles.append("All Particles")
-    if plot_components:
-        for iGauss, gauss in enumerate(gaussians):
-            if do_toy_data:
-                model.plotOn(frame,root.RooFit.Components(gauss.GetName()),root.RooFit.LineStyle(root.kDashed),root.RooFit.LineColor(COLORLIST[iGauss+1]),root.RooFit.ProjWData(toy_data,True))
-            else:
-                model.plotOn(frame,root.RooFit.Components(gauss.GetName()),root.RooFit.LineStyle(root.kDashed),root.RooFit.LineColor(COLORLIST[iGauss+1]),root.RooFit.ProjWData(data,True))
-            gaus_graphs.append(frame.getObject(int(frame.numItems())-1))
-            gaus_titles.append(gauss.GetTitle())
-    frame.Draw()
-    if plot_components:
-        leg = drawNormalLegend(gaus_graphs,gaus_titles,option="l",position=(0.55,0.7,0.85,0.89))
-    c.SaveAs("TOFFit.png")
-    c.SaveAs("TOFFit.pdf")
-
-    frame_p = true_p.frame()
-    if do_toy_data:
-        toy_data.plotOn(frame_p)
-    else:
-        data.plotOn(frame_p)
-    true_p_distribution.plotOn(frame_p)
-    frame_p.Draw()
-    c.SaveAs("TOFFit_p.png")
-    c.SaveAs("TOFFit_p.pdf")
+#
+#    gaus_graphs = []
+#    gaus_titles = []
+#    frame = mass.frame(root.RooFit.Title(""))
+#    #frame.updateNormVars(root.RooArgSet(mass,true_p)) # makes RooFit contionalize on true_p
+#    if do_toy_data:
+#        toy_data.plotOn(frame)
+#        model.plotOn(frame,root.RooFit.ProjWData(toy_data,True))
+#    else:
+#        data.plotOn(frame)
+#        model.plotOn(frame,root.RooFit.ProjWData(data,True))
+#    gaus_graphs.append(frame.getObject(int(frame.numItems())-1))
+#    gaus_titles.append("All Particles")
+#    if plot_components:
+#        for iGauss, gauss in enumerate(gaussians):
+#            if do_toy_data:
+#                model.plotOn(frame,root.RooFit.Components(gauss.GetName()),root.RooFit.LineStyle(root.kDashed),root.RooFit.LineColor(COLORLIST[iGauss+1]),root.RooFit.ProjWData(toy_data,True))
+#            else:
+#                model.plotOn(frame,root.RooFit.Components(gauss.GetName()),root.RooFit.LineStyle(root.kDashed),root.RooFit.LineColor(COLORLIST[iGauss+1]),root.RooFit.ProjWData(data,True))
+#            gaus_graphs.append(frame.getObject(int(frame.numItems())-1))
+#            gaus_titles.append(gauss.GetTitle())
+#    frame.Draw()
+#    if plot_components:
+#        leg = drawNormalLegend(gaus_graphs,gaus_titles,option="l",position=(0.55,0.7,0.85,0.89))
+#    c.SaveAs("TOFFit.png")
+#    c.SaveAs("TOFFit.pdf")
+#
+#    frame_p = true_p.frame()
+#    if do_toy_data:
+#        toy_data.plotOn(frame_p)
+#    else:
+#        data.plotOn(frame_p)
+#    true_p_distribution.plotOn(frame_p)
+#    frame_p.Draw()
+#    c.SaveAs("TOFFit_p.png")
+#    c.SaveAs("TOFFit_p.pdf")
 
 if __name__ == "__main__":
 
